01cd3791ba35676c038b9053616a4c750c39169d
anilkumar65 <ka899999@gmail.com>
2025-11-30 10:46:23 +0900

    Wrap About component in Container

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ import AboutIntro from './AboutIntro';
 
 const About = () => {
   return (
-    <>
+    <Container>
    <center> <h2 >About</h2>
     <section id="about" className="section-padding about-wrap" >
      <AboutImgSection></AboutImgSection>
@@ -13,7 +13,7 @@ const About = () => {
       
     </section>
     </center>
-    </>
+    </Container>
   );
 };
 
